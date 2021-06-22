1113635f645b6488d259072eb9a333a36b0570e3
Rainer Haering <rainer@Rainers-MacBook-Pro.local>
2021-06-22 19:33:47 +0200

    adding draft of new version

---
+++ draft/js/src/site.js
@@ -0,0 +1,73 @@
+jQuery(document).ready(function () {
+    $('.trigger-offcanvas').on('click', function (e) {
+        e.preventDefault();
+        $('.screen-darken').addClass('active');
+        $('#' + $(this).data('trigger')).addClass('show');
+        $('body').addClass('offcanvas-active');
+    })
+
+    $('.btn-close, .screen-darken').on('click', function (e) {
+        e.preventDefault();
+        $('.screen-darken').removeClass('active');
+        $('.mobile-offcanvas.show').removeClass('show');
+        $('body').removeClass('offcanvas-active');
+    })
+
+    $('#btnDeny').click(() => {
+        eraseCookie('allowCookies')
+        $('.toast').toast('hide')
+    })
+
+    $('#btnAccept').click(() => {
+        setCookie('allowCookies', '1', 7)
+        $('.toast').toast('hide')
+    })
+
+// load
+    cookieConsent()
+
+// for demo / testing only
+    $('#btnReset').click(() => {
+        // clear cookie to show toast after acceptance
+        eraseCookie('allowCookies')
+        $('.toast').toast('show')
+    })
+
+});
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function cookieConsent() {
+    console.log('pass')
+    if (!getCookie('allowCookies')) {
+        $('.toast').toast('show')
+    } else {
+        console.log('we have cookie')
+    }
+}
+
+
+
